f98906e83828697696328952b27030557754ea38
AKOTEGNON Anne-Marie <akotegnonannemarie@gmail.com>
2024-02-09 16:46:30 +0100

    add nav and header

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Nav from "./Nav";
+
+function Header(){
+    return (
+        <div>
+            <img src={logo} alt=""/>
+            <Nav/>
+        </div>
+    )
+}
---
+++ src/components/Nav.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav() {
+    return(
+        <nav>
+            <ul>
+                <li><a href="#accueil">Accueil</a></li>
+                <li><a href="#apropos">A propos</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li><a href="#services">Services</a></li>
+            </ul>
+        </nav>
+    );
+}
+export default Nav;
